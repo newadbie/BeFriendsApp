d2f8b927006f5eaf2d4248dc6fed816866ca1e0f
Adrian <bielecadriandeveloper@gmail.com>
2020-11-23 19:10:27 +0100

    Implement simple validation system for authorization

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,12 +1,13 @@
 const User = require("../models/user");
-
+const { validationResult } = require("express-validator");
 exports.postSignIn = (req, res, next) => {
+  const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
   const userMail = req.body.email;
   const userPassword = req.body.password;
-
-  if (!userMail || !userPassword) {
-    return res.status(422).json({ message: "Incorrect password or email" });
-  }
 
   const newUser = new User({
     email: userMail,
@@ -17,7 +18,7 @@ exports.postSignIn = (req, res, next) => {
     .save()
     .then(() => res.status(200).json({ message: "Correct" }))
     .catch((err) => {
-        console.log(err);
+      console.log(err);
       res.status(422).json({ message: "Opus something went wrong! Try again" });
     });
 };
--- server/routes/auth/auth.js
+++ server/routes/auth/auth.js
@@ -1,8 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+
+const { body } = require("express-validator");
 
 const authController = require("../../controllers/auth");
 
-router.post("/register", authController.postSignIn);
+router.post("/register",[
+    body('email')
+    .isEmail()
+    .exists()
+    .trim(),
+    body('password')
+    .exists()
+    .trim()
+    .isLength({min: 5}),
+    body('confirmPassword')
+    .exists()
+    .trim()
+    .custom((value, {req}) => {
+        if (value !== req.body.password) {
+            throw new Error("Passwords does not match!")
+        }
+        return true;
+    })
+],authController.postSignIn);
 
 module.exports = router;
