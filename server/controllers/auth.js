7470a9a57f35bbe2dd0e1601e2a7397c1a780355
Adrian <bielecadriandeveloper@gmail.com>
2020-11-23 18:53:31 +0100

    Simple register system, without basic user protection.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,23 @@
+const User = require("../models/user");
+
 exports.postSignIn = (req, res, next) => {
-    res.status(200).json({message: "Correct!"});
-}
+  const userMail = req.body.email;
+  const userPassword = req.body.password;
+
+  if (!userMail || !userPassword) {
+    return res.status(422).json({ message: "Incorrect password or email" });
+  }
+
+  const newUser = new User({
+    email: userMail,
+    password: userPassword,
+  });
+
+  return newUser
+    .save()
+    .then(() => res.status(200).json({ message: "Correct" }))
+    .catch((err) => {
+        console.log(err);
+      res.status(422).json({ message: "Opus something went wrong! Try again" });
+    });
+};
