8da88b733cdc0e3d39fbb030d3be469d1bfdaed3
Adrian <bielecadriandeveloper@gmail.com>
2020-11-25 14:51:36 +0100

    Create login page, implement system responsible for giving active class, for current open page. Add router.

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -2,16 +2,28 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Container from "react-bootstrap/Container";
 import Register from "./Register/Register";
+import Login from './Login/Login';
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
     <React.Fragment>
-      <Navbar />
-      <Container className="mt-5" fluid>
-        <Register />
-      </Container>
+      <Router>
+        <Navbar />
+        <Container className="mt-5" fluid>
+          <Switch>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </Container>
+      </Router>
     </React.Fragment>
   );
 };
---
+++ client/src/containers/Login/Login.js
@@ -0,0 +1,8 @@
+const Login = () => {
+
+    return (
+        <h1>Welcome at my login page!</h1>
+    )
+}
+
+export default Login;
