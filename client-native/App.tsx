1a44c016914791089d62c580b136b8917f9058cb
Adrian <bielecadriandeveloper@gmail.com>
2020-12-14 13:35:08 +0100

    !feature: init react thunk

--- client-native/App.tsx
+++ client-native/App.tsx
@@ -1,37 +1,39 @@
 import "react-native-gesture-handler";
 import * as Font from "expo-font";
 import React, { useEffect, useState } from "react";
+import thunk from "redux-thunk";
 import rootReducer from "./slices";
-import { configureStore } from "@reduxjs/toolkit";
+import { applyMiddleware, configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import { Application } from "./containers/App";
 import { StatusBar } from "react-native";
 import { Spinner } from "./components/Spinner";
 
 const App = () => {
-  const [isAppLoaded,setAppLoadedState] = useState(false);
+  const [isAppLoaded, setAppLoadedState] = useState(false);
 
-  const store = configureStore({
-    reducer: rootReducer,
-  });
+  const store = configureStore(
+    {
+      reducer: rootReducer,
+      middleware: [thunk]
+    },
+  );
   useEffect(() => {
     const loadFont = async () => {
       try {
         await Font.loadAsync({
-          Ionicons: require('./assets/Ionicons.ttf')
-        })
+          Ionicons: require("./assets/Ionicons.ttf"),
+        });
+      } catch (err) {
+        console.log(err);
       }
-      catch (err) {
-        console.log(err)
-      };
       setAppLoadedState(true);
     };
     loadFont();
   }, []);
 
-  if(!isAppLoaded)
-  {
-    return <Spinner />
+  if (!isAppLoaded) {
+    return <Spinner />;
   }
 
   return (
